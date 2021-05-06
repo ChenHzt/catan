24b27fca181ffad5b06114a66cf19fa6f719caa1
ChenHzt <chenhzt@gmail.com>
2021-05-06 18:40:21 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,18 +8,15 @@ const route = require("./routers/index");
 
 require('./db/mongoose');
 
-// const gameRouter = require('./routers/game.router');
-// const userRouter = require('./routers/user.router');
-
 app.use(express.json());
 app.use(cors());
 
 app.use(route);
 // app.use('/api/users', userRouter);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
